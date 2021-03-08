7a5cfa0a94f3d1974a25770efb0e51b13a8ff42f
Markus Amano <markus.a.amano@gmail.com>
2021-03-07 22:42:51 -0600

    made the basic classes

--- activeinventory.py
+++ activeinventory.py
@@ -1,5 +1,3 @@
-import price
-
 class User:
     def __init__(self, name = "demo"):
         self.name=name
@@ -8,10 +6,23 @@ class Inventory:
     def __init__(self, slots = 0):
         self.slots = slots
         self.objects = dict()
+    def add_object(self, obj):
+        if obj in self.objects.keys():
+            self.objects[obj] += 1
+        else:
+            self.objects[obj] = 1
+
+        print(f"You have {self.objects[obj]} {obj}(s)")
+
+def worthless_amount():
+    return Price(price = {"cp":0,"sp":0,"ep":0,"gp":0,"pp":0})
+
+class Price:
+    def __init__(self, price):
+        self.price = price
 
 class Object:
-    def __init__(self, name = "demo", bulk = 0, price = price.zero()):
+    def __init__(self, name = "demo", bulk = 0, price = worthless_amount()):
         self.name = name
         self.bulk = bulk
         self.price = price
-
